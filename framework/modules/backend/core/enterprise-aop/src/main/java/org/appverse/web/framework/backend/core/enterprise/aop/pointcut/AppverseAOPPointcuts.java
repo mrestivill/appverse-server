66b842e018e0ac07c780f8129d1b8f764a3b69e3
Roger Bofarull <roger.bofarull@gft.com>
2015-04-22 13:18:57 +0200

    generic pointcuts

--- framework/modules/backend/core/enterprise-aop/src/main/java/org/appverse/web/framework/backend/core/enterprise/aop/pointcut/AppverseAOPPointcuts.java
+++ framework/modules/backend/core/enterprise-aop/src/main/java/org/appverse/web/framework/backend/core/enterprise/aop/pointcut/AppverseAOPPointcuts.java
@@ -1,5 +1,48 @@
 package org.appverse.web.framework.backend.core.enterprise.aop.pointcut;
 
+import org.aspectj.lang.annotation.Pointcut;
+
+/**
+ * Appverse predefined pointcuts that can be used in aop configuration file.
+ * 
+ * @author RRBL
+ *
+ */
 public class AppverseAOPPointcuts {
+
+	/**
+	 * Pointcut for all classes extending AbstractPresentationService
+	 */
+	@Pointcut("execution(public * org.appverse.web.framework.backend.core.services.AbstractPresentationService+.*(..))")
+	public void allPresentationServices() {
+	}
+
+	/**
+	 * Pointcut for all classes extending AbstractBusinessService
+	 */
+	@Pointcut("execution(public * org.appverse.web.framework.backend.core.services.AbstractBusinessService+.*(..))")
+	public void allBusinessServices() {
+	}
+
+	/**
+	 * Pointcut for all classes extending spring data Repository
+	 */
+	@Pointcut("execution(public * org.springframework.data.repository.Repository+.*(..))")
+	public void allIntegrationDataServices() {
+	}
+
+	/**
+	 * Pointcut grouping presentation and business services
+	 */
+	@Pointcut("allPresentationServices() || allBusinessServices()")
+	public void allPresentationOrBusinessServices() {
+	}
+
+	/**
+	 * Pointcut for all services in all layers
+	 */
+	@Pointcut("allPresentationServices() || allBusinessServices() || allIntegrationDataServices()")
+	public void allServicesCalls() {
+	}
 
 }
