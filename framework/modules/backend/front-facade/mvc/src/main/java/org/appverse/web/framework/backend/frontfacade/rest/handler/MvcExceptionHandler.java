70d12d9bdf76462e42facb3e866588dd5da36902
Rodriguez Estivill, Marc <mrodriguezestivill@gmail.com>
2015-06-11 16:30:06 +0200

    APPS-102 created MvcExceptionHandler with test, Added asserts on JerseyExceptionHandler

---
+++ framework/modules/backend/front-facade/mvc/src/main/java/org/appverse/web/framework/backend/frontfacade/rest/handler/MvcExceptionHandler.java
@@ -0,0 +1,57 @@
+package org.appverse.web.framework.backend.frontfacade.rest.handler;/*
+ Copyright (c) 2012 GFT Appverse, S.L., Sociedad Unipersonal.
+
+ This Source Code Form is subject to the terms of the Appverse Public License 
+ Version 2.0 (“APL v2.0”). If a copy of the APL was not distributed with this 
+ file, You can obtain one at http://www.appverse.mobi/licenses/apl_v2.0.pdf. [^]
+
+ Redistribution and use in source and binary forms, with or without modification, 
+ are permitted provided that the conditions of the AppVerse Public License v2.0 
+ are met.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. EXCEPT IN CASE OF WILLFUL MISCONDUCT OR GROSS NEGLIGENCE, IN NO EVENT
+ SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) 
+ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import org.appverse.web.framework.backend.core.AbstractException;
+import org.appverse.web.framework.backend.frontfacade.rest.beans.ErrorVO;
+import org.appverse.web.framework.backend.frontfacade.rest.beans.ResponseDataVO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice(annotations = {RestController.class, Controller.class})
+public class MvcExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ResponseDataVO> handleError(HttpServletRequest req, Exception exception) {
+        ResponseDataVO data = new ResponseDataVO();
+        ErrorVO error = new ErrorVO();
+
+        Long code = 500L;
+        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        if(exception instanceof AbstractException) {
+            if (((AbstractException) exception).getCode() != null) {
+                code = ((AbstractException) exception).getCode();
+            }
+        }
+        error.setCode(code);
+        error.setMessage(exception.getMessage());
+        data.setErrorVO(error);
+        return  new ResponseEntity<ResponseDataVO>(data, httpStatus);
+    }
+}
--- framework/modules/backend/front-facade/mvc/src/test/java/org/appverse/web/framework/backend/frontfacade/rest/MvcExceptionHandlerTests.java
+++ framework/modules/backend/front-facade/mvc/src/test/java/org/appverse/web/framework/backend/frontfacade/rest/MvcExceptionHandlerTests.java
@@ -1,11 +1,5 @@
 package org.appverse.web.framework.backend.frontfacade.rest;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import javax.annotation.PostConstruct;
-
-import org.appverse.web.framework.backend.frontfacade.rest.TestExceptionHandlingResource.SimpleBean;
 import org.appverse.web.framework.backend.frontfacade.rest.beans.ResponseDataVO;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,10 +13,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = {FrontFacadeModuleTestsConfigurationApplication.class})
-@WebIntegrationTest(randomPort= true, value={"security.basic.enabled=false","debug=true"/*,"server.servlet-path=/spring"*/})
-public class JerseyExceptionHandlerTests {
+@WebIntegrationTest(randomPort= true, value={"security.basic.enabled=false","debug=true",
+        "appverse.frontfacade.rest.remoteLogEndpoint.enabled=false",
+        "appverse.frontfacade.rest.basicAuthenticationEndpoint.enabled=false",
+        "appverse.frontfacade.rest.simpleAuthenticationEndpoint.enabled=false"})
+public class MvcExceptionHandlerTests {
 	@Autowired
 	private AnnotationConfigEmbeddedWebApplicationContext context;
 	
@@ -41,7 +43,7 @@ public class JerseyExceptionHandlerTests {
     public void test() {
     	int port = context.getEmbeddedServletContainer().getPort();
         ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port + "/api/hello", String.class);
-    	String data = response.getBody();
+        String data = response.getBody();
         assertEquals("Hello World!", data);
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
@@ -53,9 +55,10 @@ public class JerseyExceptionHandlerTests {
     public void testExceptionHandler() {
     	int port = context.getEmbeddedServletContainer().getPort();
         ResponseEntity<ResponseDataVO> response = restTemplate.getForEntity("http://localhost:"+port+"/api/exc", ResponseDataVO.class);
-    	ResponseDataVO data = response.getBody();
-    	assertEquals(500, data.getErrorVO().getCode());
+        ResponseDataVO data = response.getBody();
+        assertEquals(500, data.getErrorVO().getCode());
         assertTrue("contains message", data.getErrorVO().getMessage().contains("kk"));
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+
     }
 }
