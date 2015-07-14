37f8376211fcb49b187adea552695228a1e81460
Miguel Fernandez <miguel.fernandez@gft.com>
2015-07-14 10:37:23 +0200

    Polishing code in OAuth2 module

--- framework/modules/backend/security/oauth2/src/main/java/org/appverse/web/framework/backend/security/oauth2/configuration/jdbcstore/ResourceServerWithJDBCStoreConfigurerAdapter.java
+++ framework/modules/backend/security/oauth2/src/main/java/org/appverse/web/framework/backend/security/oauth2/configuration/jdbcstore/ResourceServerWithJDBCStoreConfigurerAdapter.java
@@ -31,7 +31,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 /**
  * Convenient setup for an OAuth2 Resource Server that uses a
--- framework/modules/backend/security/oauth2/src/main/java/org/appverse/web/framework/backend/security/oauth2/handlers/OAuth2LogoutHandler.java
+++ framework/modules/backend/security/oauth2/src/main/java/org/appverse/web/framework/backend/security/oauth2/handlers/OAuth2LogoutHandler.java
@@ -1,3 +1,26 @@
+/*
+ Copyright (c) 2012 GFT Appverse, S.L., Sociedad Unipersonal.
+
+ This Source Code Form is subject to the terms of the Appverse Public License 
+ Version 2.0 (“APL v2.0”). If a copy of the APL was not distributed with this 
+ file, You can obtain one at http://www.appverse.mobi/licenses/apl_v2.0.pdf. [^]
+
+ Redistribution and use in source and binary forms, with or without modification, 
+ are permitted provided that the conditions of the AppVerse Public License v2.0 
+ are met.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. EXCEPT IN CASE OF WILLFUL MISCONDUCT OR GROSS NEGLIGENCE, IN NO EVENT
+ SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) 
+ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.appverse.web.framework.backend.security.oauth2.handlers;
 
 import java.io.IOException;
@@ -7,7 +30,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-// import org.apache.commons.lang.StringUtils;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.token.TokenStore;
@@ -15,6 +37,14 @@ import org.springframework.security.web.authentication.AbstractAuthenticationTar
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
 
+/**
+ * This logout handler will just invalidate the OAuth2 token that was granted in the
+ * authentication endpoint when the user wants to logout.
+ * The concept of 'login' and 'logout' with OAuth2 is a bit 'weird' or little natural
+ * as most of the times OAuth2 is used in stateless applications (which is one of its
+ * biggest advantages). So in this case we just remove the token from the token store
+ * and we return HTTP OK.
+ */
 @Component
 public class OAuth2LogoutHandler extends
 		AbstractAuthenticationTargetUrlRequestHandler implements
