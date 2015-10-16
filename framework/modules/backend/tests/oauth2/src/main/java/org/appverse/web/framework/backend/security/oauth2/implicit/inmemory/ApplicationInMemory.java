8a403d70c9705b8cba93d924e03bf60ffec6be16
Miguel Fernandez <miguel.fernandez@gft.com>
2015-10-16 14:48:57 +0200

    Avoid bean initialization clash with tests

--- framework/modules/backend/tests/oauth2/src/main/java/org/appverse/web/framework/backend/security/oauth2/implicit/inmemory/ApplicationInMemory.java
+++ framework/modules/backend/tests/oauth2/src/main/java/org/appverse/web/framework/backend/security/oauth2/implicit/inmemory/ApplicationInMemory.java
@@ -21,7 +21,7 @@
  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
  POSSIBILITY OF SUCH DAMAGE.
  */
-package org.appverse.web.framework.backend.security.oauth2;
+package org.appverse.web.framework.backend.security.oauth2.implicit.inmemory;
 
 import org.appverse.web.framework.backend.security.oauth2.implicit.configuration.memory.AuthorizationServerInMemoryStoreConfigurerAdapter;
 import org.springframework.boot.SpringApplication;
--- framework/modules/backend/tests/oauth2/src/test/java/org/appverse/web/framework/backend/security/oauth2/implicit/jdbc/Oauth2RESTProtectedAPIWithImplicitFlowJdbcTests.java
+++ framework/modules/backend/tests/oauth2/src/test/java/org/appverse/web/framework/backend/security/oauth2/implicit/jdbc/Oauth2RESTProtectedAPIWithImplicitFlowJdbcTests.java
@@ -21,9 +21,9 @@
  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
  POSSIBILITY OF SUCH DAMAGE.
  */
-package org.appverse.web.framework.backend.security.oauth2.implicit;
+package org.appverse.web.framework.backend.security.oauth2.implicit.jdbc;
 
-import org.appverse.web.framework.backend.security.oauth2.ApplicationJdbc;
+import org.appverse.web.framework.backend.security.oauth2.implicit.jdbc.ApplicationJdbc;
 import org.appverse.web.framework.backend.test.util.oauth2.tests.predefined.implicit.Oauth2ImplicitFlowPredefinedTests;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
